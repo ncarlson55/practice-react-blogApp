d93e7332be2afaf705ddbde2de609220900994ce
Nico Carlson <nicocarlson285@gmail.com>
2021-05-13 22:31:42 -0500

    Exact Match Routes added, passing id from our object to the link components we are using from react-router-dom

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from "./Navbar"
 import Home from './Home'
 import Create from './Create'
 import { BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import BlogDetails from "./BlogDetails";
 
 function App() {
 
@@ -19,6 +20,10 @@ function App() {
             {/* Create Path Route */}
             <Route path="/create">
               <Create />
+            </Route>
+            {/* Blog Details */}
+            <Route exact path="/blogs/:id" >
+              <BlogDetails />
             </Route>
           </Switch>
         </div>
