089c8a2d187f915521d5ae3da947e38b09a8cd58
Nico Carlson <nicocarlson285@gmail.com>
2021-05-14 22:31:38 -0500

    404 page added, App complete

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './Home'
 import Create from './Create'
 import { BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import BlogDetails from "./BlogDetails";
+import NotFound from './NotFound'
 
 function App() {
 
@@ -24,6 +25,9 @@ function App() {
             {/* Blog Details */}
             <Route exact path="/blogs/:id" >
               <BlogDetails />
+            </Route>
+            <Route path='*'>
+              <NotFound />
             </Route>
           </Switch>
         </div>
